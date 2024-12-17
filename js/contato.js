f58dfb6e165f043265185f4c3e0130a9fe5459b9
sx-nt <ssemeghini.santana@gmail.com>
2024-12-17 20:08:10 -0300

    auauau

--- js/contato.js
+++ js/contato.js
@@ -4,6 +4,11 @@ const form = document.querySelector('.contato__formulario');
 const aviso = document.querySelector('.contato__texto');
 
 const contatos = [];
+const preenchido = JSON.parse(localStorage.getItem('preenchido')) || {};
+
+for (let id in preenchido) {
+    document.querySelector(`#${id}`).value = preenchido[id];
+}
 
 form.addEventListener('submit', evento => {
     evento.preventDefault();
@@ -24,6 +29,8 @@ form.addEventListener('submit', evento => {
     
     const contato = {nome, email, periodo, mensagem};
     contatos.push(contato);
+
+    localStorage.setItem('contatos', JSON.stringify(contatos));
    
     console.log(contatos);
     
@@ -32,5 +39,11 @@ form.addEventListener('submit', evento => {
     
     aviso.textContent = "Responderemos depois da pescaria.";
     aviso.classList.remove('cuidado');
+
+    localStorage.removeItem('preenchido');
 });
 
+form.addEventListener("change", evento => {
+    preenchido[evento.target.id] = evento.target.value;
+    localStorage.setItem('preenchido', JSON.stringify(preenchido));
+})
---
+++ js/tabela.js
@@ -0,0 +1,16 @@
+const tabela = document.createElement('table');
+tabela.classList.add('contatos__tabela');
+
+const tbody = document.createElement('tbody')
+tbody.classList.add('contatos__tabela');
+
+const tr = document.createElement('tr');
+tr.classList.add('contatos__tabela');
+
+const th = document.createElement('th');
+th.classList.add('contatos__tabela');
+
+
+
+//tabelaContatos.textContent = contatos[contato].tabelaContatos;
+
